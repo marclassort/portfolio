2c0d5ed46c79cec7146d032796d3220c947fbc5f
marc <mlassort@acfi-network.com>
2024-05-22 17:32:44 +0200

    Update names.

--- tests/unit/sections/hero.test.jsx
+++ tests/unit/sections/hero.test.jsx
@@ -8,7 +8,7 @@ describe('Hero section parts', () => {
 		render(<Hero />)
 		
 		const heading = screen.getByRole('heading', {
-			name: /Andrew Nelson\./i,
+			name: /Shinkyôkai\./i,
 		})
 		
 		expect(heading).toBeInTheDocument()
